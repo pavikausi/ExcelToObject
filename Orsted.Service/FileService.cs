35ef68b0d2d34f129b7afab07a4057e30cfba090
pavikausi <97398913+pavikausi@users.noreply.github.com>
2022-01-10 11:45:20 +0800

    Service class

---
+++ Orsted.Service/FileService.cs
@@ -0,0 +1,17 @@
+﻿namespace Orsted.Service
+{
+    public class FileService : BaseFileService
+    {
+        public override bool CheckFileExtension(string fileExtension, string[] extensions)
+        {
+            foreach (var extension in extensions)
+            {
+                if (fileExtension == extension)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
